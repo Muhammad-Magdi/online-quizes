d23a12851ce54315177b4cdeb3772f4c5b34fca9
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-08-29 00:04:44 +0200

    add the delete-question Endpoint

--- src/routers/quiz.js
+++ src/routers/quiz.js
@@ -3,7 +3,6 @@ const Quiz = require('../models/quiz');
 
 const router = new express.Router();
 
-// TODO add Authentication
 router.post('/quizes', async (req, res) => {
   const quiz = new Quiz(req.body);
   try {
@@ -46,6 +45,23 @@ router.get('/quizes/:id', async ({ params: { id } }, res) => {
     res.status(200).send(quiz);
   } catch (e) {
     res.status(500).send(e);
+  }
+});
+
+// I think this is not the best way
+// It fetches the whole quiz to delete a question
+// But it works for now
+router.delete('/quizes/:id', async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.id);
+    if (!quiz) {
+      throw new Error('Invalid Quiz id!');
+    }
+    quiz.questions.id(req.body._id).remove();
+    await quiz.save();
+    res.status(200).send(quiz);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
